45877e9a30df8525046e277915e1339cc0d9fa7e
Qwreey <dev@qwreey.kr>
2023-06-03 01:54:20 +0900

    Updated for windows compatibility

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import datetime
 import pandas
 import aioconsole
 import matplotlib.pyplot as pyplot
-import matplotlib
 import math
-import os
 import subprocess
+import sys
 
+# -----------------------------------------------------------------------------------
 # 설정
 # 시리얼 포트(윈도우: 디바이스포트명 COM1 COM2 COM3, 리눅스: /dev/ttyS0 ...)
 SERIAL_PORT="COM8"
@@ -17,18 +17,24 @@ SERIAL_PORT="COM8"
 # 보드레이트 bps (Serial.begin 에 넣은만큼 설정)
 BAUDRATE=9600
 
-# 윈도우에서 코드를 실행하는 경우 True 로 바꿔주세요
-DISABLE_GRAPH=False
-DISABLE_AIOCONSOLE=True
-
-# 아두이노 없는 상황에서 테스트를 위한 부분
+# 아두이노 없는 상황에서 테스트를 위한 부분, 사인 함수로 대신합니다
 TEST_MODE=True
+
+# 그래프를 보여주지 않습니다
+DISABLE_GRAPH=False
+
+# 아두이노의 이전값을 불러와 오차가 발생함을 방지하기 위해서, 프로그램 실행 후 무시될 수 있는 첫 결과 수
+SKIP_FIRST_RESULT=10
+
+# 서브프로세스를 이용해 그래프를 그립니다. 윈도우의 경우 이 옵션이 활성화되어있지 않으면 윈도우 환경에서 매우 느린 렌더속도를 경험하게 됩니다
+SUBPROCESS_GRAPH=True
+
+# 호환성을 위해 AsyncIO Console 라이브러리를 비활성화합니다
+DISABLE_AIOCONSOLE=True
+# -----------------------------------------------------------------------------------
 
 # https://stackoverflow.com/questions/55409641/asyncio-run-cannot-be-called-from-a-running-event-loop-when-using-jupyter-no
 # 다른 스레드에서 async 함수를 asyncio 로 진행시킵니다
-# 이는... 파이썬 언어의 async 구조적 한계(?) 점에 의해 이렇게 더러운것이므로
-# 요 부분은 이해하려고 하지 않는게 편합니다
-# (async 가 파이썬에서 가장 어려운? 난해한? 부분이라....)
 class RunThread(threading.Thread):
     def __init__(self, func, args, kwargs):
         self.func = func
@@ -48,13 +54,21 @@ def run_async(func, *args, **kwargs):
 # 자세한 사항은 pyserial asyncio 라이브러리 문서 참조
 # : https://pyserial-asyncio.readthedocs.io/en/latest/shortintro.html#protocol-example
 class Arduino:
+    def __init__(self,test,callback):
+        self.callback = callback
+        self.test = test
+        self.killed = False
+
+    # 테스트용 루프
     async def testLoop(self,serial_port,baudrate):
         time=0
         while True:
-            await self.callback("{},{}\n".format(int(time),int(math.sin(time/100*3.14)*20)))
-            time+=1
-            asyncio.sleep(1)
+            await self.callback("{},{}\n".format(int(time),int(math.sin(time/10000*3.14)*16+20)))
+            time+=100
+            await asyncio.sleep(0.1)
             if self.killed: break
+
+    # 아두이노에서 값 읽어오는 루프
     async def loop(self,serial_port,baudrate):
         self.reader, self.writer = await serial_asyncio.open_serial_connection(url=serial_port, baudrate=baudrate)
         while True:
@@ -62,28 +76,42 @@ class Arduino:
             if self.reader.at_eof(): continue
             try: await self.callback((await self.reader.readline()).decode("utf-8"))
             except: pass
+
+    # 아두이노와 연결
     def connect(self,serial_port,baudrate):
         self.thread = run_async(self.test and self.testLoop or self.loop,serial_port,baudrate)
+
+    # 연결 해제
     def kill(self):
         self.killed = True
         if self.test: return
         self.thread.join(3)
-    def __init__(self,test,callback):
-        self.callback = callback
-        self.test = test
-        self.killed = False
 
 # 그래프를 그립니다
 class GraphHandler:
-    def __init__(self):
-        self.process = subprocess.Popen(["C:\\Windows\\py.exe", "plotprocess.py"], stdin=subprocess.PIPE)
-    def animate(self,x,y):
-        self.process.stdin.write(("{},{}\n".format(x,y)).encode())
-    def kill(self):
-        self.process.kill()
+    def __init__(self,usingSubprocess):
+        # 서브프로세스를 요구하는 경우 생성
+        if usingSubprocess:
+            self.process = subprocess.Popen([sys.executable, "plotprocess.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            self.process.stdin.write(b"READY\n")
+            self.process.stdin.flush()
+            self.process.stdout.readline()
+            self.usingSubprocess = True
+            return
 
-"""
+        # 일반 스레드에서 생성
+        self.bufferX, self.bufferY, self.length = [],[],0
+        self.fig, self.ax = pyplot.subplots()
+        pyplot.set_loglevel('error')
+        pyplot.show(block=False)
+
     def animate(self, x, y):
+        # 서브프로세스가 있는경우 작업을 넘겨줌
+        if self.usingSubprocess:
+            self.process.stdin.write(("{},{}\n".format(x,y)).encode())
+            self.process.stdin.flush()
+            return
+
         # 최근 20 초의 기록만 보여줌
         self.bufferX.append(x)
         self.bufferY.append(y)
@@ -92,15 +120,23 @@ class GraphHandler:
             self.length -= 1
             del self.bufferX[0]
             del self.bufferY[0]
-        self.ax.clear()
+
+        # 렌더링을 수행함
         self.ax.set_ylim(0,40)
         self.ax.plot(self.bufferX, self.bufferY)
-        pyplot.draw()
+        self.fig.canvas.draw()
         self.fig.canvas.flush_events()
-"""
-        
+
+    # 서브프로세스가 있으면 죽입니다
+    def kill(self):
+        if self.usingSubprocess:
+            self.process.kill()
+
 # 엑셀 파일을 작성합니다
 class FileHandler:
+    def __init__(self):
+        self.readTemplate()
+
     # 시간을 기준으로 출력 파일 이름을 생성합니다
     def makeFilename(self):
         now = datetime.datetime.now()
@@ -120,15 +156,13 @@ class FileHandler:
         with pandas.ExcelWriter(fileName) as w:
             self.file.to_excel(w, sheet_name='Result', index=False)
         return fileName
-    def __init__(self):
-        self.readTemplate()
 
 # 콘솔에서 값이 입력됨을 감지합니다
 class Readline:
     async def loop(self,callback):
         while True:
             if DISABLE_AIOCONSOLE:
-                await callback(input(''))
+                await callback(sys.stdin.readline())
             else:
                 await callback(await aioconsole.ainput(''))
     def __init__(self,callback):
@@ -139,18 +173,19 @@ class Readline:
 async def main():
     # 시작 시간을 따로 기록해, 기록이 시작된 시점을 t=0 으로 만들어줍니다
     startAt = False
-    skipFirstByte = 0 # 연결전 쓰래기 값을 제거하기 위해 첫번째 20 개의 결과를 스킵합니다
+    skipFirst = 0 # 연결전 쓰래기 값을 제거하기 위해 첫번째 20 개의 결과를 스킵합니다
     fileHandler = FileHandler()
-    if not DISABLE_GRAPH: graph = GraphHandler()
+    if not DISABLE_GRAPH: graph = GraphHandler(SUBPROCESS_GRAPH)
 
     # 아두이노에서 읽은 데이터를 , 을 기준으로 나눠줍니다
     # 그런 후, 메모리에 집어넣고 그래프로 표시합니다
     async def arduinoCallback(str:str):
         nonlocal startAt
-        nonlocal skipFirstByte
+        nonlocal skipFirst
 
-        if skipFirstByte<20:
-            skipFirstByte += 1
+        # 첫 항목 무시
+        if skipFirst<SKIP_FIRST_RESULT:
+            skipFirst += 1
             return
 
         # 0 번째 : 시간 (ms)
@@ -180,6 +215,7 @@ async def main():
     # 메모리에 있던 데이터들을 엑셀로 저장합니다
     async def inputCallback(str:str):
         arduino.kill()
+        graph.kill()
         print("\033[K\r파일 저장중 . . .")
         print("파일이 "+fileHandler.save()+" 에 저장되었습니다")
         print("종료합니다")
--- plotprocess.py
+++ plotprocess.py
@@ -1,49 +1,44 @@
-import subprocess
 import matplotlib.pyplot as plt
 import sys
-from matplotlib.animation import FuncAnimation
 
-plt.set_loglevel('error')
 fig, ax = plt.subplots()
-plt.show(block=False)
-plt.pause(0.1)
-bg = fig.canvas.copy_from_bbox(fig.bbox)
-
 x, y, length = [], [], 0
-(line,) = ax.plot(x, y, animated=True)
-fig.canvas.blit(fig.bbox)
 
-fig.canvas.restore_region(bg)
+plt.show(block=False)
+(line,) = ax.plot(x, y)
 
-self.thread = run_async(self.loop)
-    def animate(self, x, y):
-        # 최근 20 초의 기록만 보여줌
-        self.bufferX.append(x)
-        self.bufferY.append(y)
-        self.length += 1
-        if self.length > 200:
-            self.length -= 1
-            del self.bufferX[0]
-            del self.bufferY[0]
-        self.ax.clear()
-        self.ax.set_ylim(0,40)
-        self.
-        pyplot.draw()
+plt.pause(.1)
 
+def append(newX,newY):
+    global length, x, y
+    x.append(float(newX))
+    y.append(float(newY))
+    length += 1
+    if length > 200:
+        length -= 1
+        del x[0]
+        del y[0]
 
-def update(frame):
-    data = sys.stdin.readline()
-    
-fig.canvas.flush_events()
-    
-
-    x.append(frame)
-    y.append(np.sin(frame))
+def updateLine():
+    global line, ax
     line.set_data(x, y)
-    return line,
+    ax.set_ylim(0,40)
+    last = x[len(x)-1]
+    ax.set_xlim(last-20,last)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
 
+updatedCount = 0
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128))
-plt.show()
+while True:
+    stdinStr = sys.stdin.readline()
+    if stdinStr == "READY\n":
+        sys.stdout.write("OK\n")
+        sys.stdout.flush()
+        continue
 
-subprocess.Popen(['C:\Windows\py.exe','f'],stdin=subprocess.PIPE)
+    datas = stdinStr.removesuffix("\n").split(",")
+    if len(datas) != 2: continue
+    append(datas[0],datas[1])
+    if updatedCount%2 == 0: updateLine()
+    updatedCount += 1
